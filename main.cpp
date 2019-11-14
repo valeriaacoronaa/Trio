a73640b051ee208449f2b894c6d67e20ae46dbf4
valeriaacoronaa <vcorona@mail.csuchico.edu>
2019-11-13 17:41:26 -0800

    rearranges three values so they are in order from greatest to least

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 // <-- ADD YOUR FUNCTION PROTOTYPE HERE
+void sortNum(int& , int&, int&);
 
 int main()
 {
@@ -13,7 +14,7 @@ int main()
 
 
   // <-- ADD YOUR FUNCTION CALL HERE
-
+  sortNum(red, green, blue);
 
   //DO NOT CHANGE WITHIN THIS AREA...
   cout<<"Rearranged....\n";
@@ -23,3 +24,51 @@ int main()
 }
 
 // <-- ADD YOUR FUNCTION DEFINITON HERE
+void sortNum(int& red, int& green, int& blue)
+{
+  int greatest;
+  int middle;
+  int least;
+
+  if(red<blue&&blue<green)
+  {
+    greatest=green;
+    middle=blue;
+    least=red;
+  }
+  else if(blue<red&&red<green)
+  {
+    greatest=green;
+    middle=red;
+    least=blue;
+  }
+  else if(green<blue&&blue<red)
+  {
+    greatest=red;
+    middle=blue;
+    least=green;
+  }
+  else if(green<red&&red<blue)
+  {
+    greatest=blue;
+    middle=red;
+    least=green;
+  }
+  else if(blue<green&&green<red)
+  {
+    greatest=red;
+    middle=green;
+    least=blue;
+  }
+  else if(red<green&&green<blue)
+  {
+    greatest=blue;
+    middle=green;
+    least=red;
+  }
+ red=greatest;
+ green=middle;
+ blue=least;
+
+}    
+
